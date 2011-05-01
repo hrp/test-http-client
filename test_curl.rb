2c8988e68856b1a6b3b423eb2c3a63d770cfeda9
Jack Chu <kamuigt@gmail.com>
2011-05-01 17:24:13 -0400

    curl: move header setting outside of test loop

--- test_curl.rb
+++ test_curl.rb
@@ -1,7 +1,8 @@
+header = {"X-Test" => "test"}
+header_str = ""
+header.each { |key, value| header_str += "-H \"#{key}: #{value}\" "}
+
 test_http("curl") do
-  header = {"X-Test" => "test"}
-  header_str = ""
-  header.each { |key, value| header_str += "-H \"#{key}: #{value}\" "}
   response = `curl #{header_str} -s #{URL_STRING}`
   verify_response(response)
 end
