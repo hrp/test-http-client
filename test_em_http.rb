b9092715f3489fc704b60104e4f525a8a10fe023
Jack Chu <kamuigt@gmail.com>
2011-05-01 23:18:58 -0400

    fix em test so that the entire em loop is in the test loop.

--- test_em_http.rb
+++ test_em_http.rb
@@ -1,9 +1,14 @@
 evm_count = ITERATIONS
 test_http("em-http") do
-  http = EventMachine::HttpRequest.new(URL_STRING).get \
-    :head => {"X-Test" => "test"}, :timeout => 60
+  EventMachine.run do
+    http = EventMachine::HttpRequest.new(URL_STRING).get \
+      :head => {"X-Test" => "test"}, :timeout => 60
 
-  http.callback do
-    verify_response(http.response)
+    http.errback { EventMachine.stop }
+
+    http.callback do
+      verify_response(http.response)
+      EventMachine.stop
+    end
   end
 end
