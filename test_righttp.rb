16f347143d224817ac33d0d8aaa1cfb601dbf190
Jack Chu <kamuigt@gmail.com>
2011-05-01 22:52:12 -0400

    righttp: move request initialization outside test loop

--- test_righttp.rb
+++ test_righttp.rb
@@ -1,11 +1,12 @@
+request  = Rig::HTTP.new(
+  :host   => URL_HOST,
+  :port   => URL_PORT,
+  :path   => URL_PATH,
+  :method => "GET",
+  :header => { "X-Test" => "test" }
+)
+
 test_http("righttp") do
-  request  = Rig::HTTP.new(
-    :host   => URL_HOST,
-    :port   => URL_PORT,
-    :path   => URL_PATH,
-    :method => "GET",
-    :header => { "X-Test" => "test" }
-  )
   resp = request.send
   verify_response(resp.body)
 end
