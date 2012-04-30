88e816d0fded5467c7da581060c24833b72f484c
Jonathan Rochkind <jonathan@dnil.net>
2012-04-30 17:01:54 -0400

    add CONCURRENCY feature

--- test.rb
+++ test.rb
@@ -11,6 +11,12 @@ PATH = ARGV.shift
 FILES = ARGV.shift || "test_*.rb"
 TESTS = []
 
+# Will do CONCURRENCY requests concurrently in
+# in ruby threads. If 0, all tests done in main
+# thread. If 1, only one at a time, but in seperate
+# thread. More than 1, well, you see how it goes. 
+CONCURRENCY = (ENV['CONCURRENCY'] || 0).to_i
+
 def test_http(name, &block)
   TESTS << [name, block]
 end
@@ -24,13 +30,32 @@ Dir[File.join(dir, FILES)].each do |file|
 end
 
 at_exit do
+  outer_loop_iterations = if CONCURRENCY == 0
+     ITERATIONS
+   else
+     ITERATIONS / CONCURRENCY
+   end
+ 
+  
   puts "Execute http performance test using ruby #{RUBY_DESCRIPTION}"
-  puts "  doing #{ITERATIONS} request in each test..."
+  puts "  doing #{ITERATIONS} requests (#{outer_loop_iterations} iterations with concurrency of #{CONCURRENCY}) in each test..."
   Benchmark.bm(28) do |x|
     for name, block in TESTS do
       begin
         x.report("testing #{name}") do
-          ITERATIONS.times(&block)
+          outer_loop_iterations.times do
+            if CONCURRENCY == 0
+              block.call
+            else
+              threads = []
+              CONCURRENCY.times do
+                threads << Thread.new do
+                  block.call
+                end
+              end
+              threads.each {|t| t.join}
+            end
+          end
         end
       rescue => ex
         puts " --> failed #{ex}"
