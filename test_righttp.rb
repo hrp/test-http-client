e8437763d6f715d0e0bd034000b62185a37adf42
Chetan Sarva <chetan@pixelcop.net>
2012-05-05 00:52:33 -0400

    righttp: move request initialization outside test loop

--- test_righttp.rb
+++ test_righttp.rb
@@ -1,12 +1,12 @@
+rig_req   = Rig::HTTP.new(
+  :host   => URL.host,
+  :port   => URL.port,
+  :path   => URL.path,
+  :method => "GET",
+  :header => { "X-Test" => "test" }
+)
 test_http("righttp") do
-  request  = Rig::HTTP.new(
-    :host   => URL.host,
-    :port   => URL.port,
-    :path   => URL.path,
-    :method => "GET",
-    :header => { "X-Test" => "test" }
-  )
-  resp = request.send
+  resp = rig_req.send
   data = MultiJson.load(resp.body)
   raise Exception.new unless data.first["number"] != 123123
 end
